579a61ad015d6ea6b0c39625238c9497322146ae
D1M1MAGS <53001378+D1M1MAGS@users.noreply.github.com>
2019-11-27 16:34:53 +1100

    first commit

---
+++ src/app/models/task.interface.ts
@@ -0,0 +1,5 @@
+export interface Task{
+    start:number;
+    name:string;
+    stop:number;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {timer, Subscription, VirtualTimeScheduler } from 'rxjs';
+
+import { DataService } from '../data.service';
+//import {Task} from '../models/task.interface';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  
+  taskForm: FormGroup;
+  timerSub: Subscription;
+  time: number;
 
-  constructor() {}
+  constructor(
+    private formBuilder:FormBuilder,
+    private dataService : DataService
+  ) {  }
+
+  ngOnInit(){
+    this.taskForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(1)]]
+    });
+  }
 
 }
